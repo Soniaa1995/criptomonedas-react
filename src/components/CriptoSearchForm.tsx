a331082c0979134049788c3987864a38c9d3e755
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-08-09 15:36:14 +0200

    mostrando las criptomonedas

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,6 +1,10 @@
 import { currencies } from "../data"
+import { useCryptoStore } from "../store"
 
 export default function CriptoSearchForm() {
+
+    const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies) //extraer el state de nuestra store
+
   return (
     <form className="form">
         <div className="field">
@@ -28,6 +32,13 @@ export default function CriptoSearchForm() {
                 id="criptoCurrency"
             >
                 <option value="">Seleccione</option>
+                {cryptocurrencies.map(crypto => (
+                    <option
+                        key={crypto.CoinInfo.FullName}
+                        value={crypto.CoinInfo.Name}>
+                            {crypto.CoinInfo.FullName}
+                    </option>
+                ))}
                 
             </select>
         </div>
