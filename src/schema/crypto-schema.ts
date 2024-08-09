1049afca1b15809e5346a521adf2323f61ae659b
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-08-09 15:24:30 +0200

    generando la funcion GET para obtener las monedas con axios + su instalacion

--- src/schema/crypto-schema.ts
+++ src/schema/crypto-schema.ts
@@ -5,3 +5,12 @@ export const CurrencySchema = z.object({
     code: z.string(),
     name: z.string()
 })
+
+export const CryptoCurrencyResponseSchema = z.object({
+    CoinInfo : z.object({ //se ponen los nombres tal cual aparecen en el log
+        FullName: z.string(),
+        Name: z.string()
+    })
+})
+
+export const CryptoCurrenciesResponseSchema = z.array(CryptoCurrencyResponseSchema)  //coge es eesquema y lo convierte ne un arreglo
